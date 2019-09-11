e69896fb2bd4f141cbcaab5ddf51ecdf5df2d9e0
Max <xewe@MacBook.local>
2019-09-10 19:25:34 -0700

    commit 3

--- src/main/java/edu/sbcc/cs105/Program.java
+++ src/main/java/edu/sbcc/cs105/Program.java
@@ -1,10 +1,34 @@
-// Replace all code in this file with what is provided in the assignment
+/**
+ * CS 105 Theory and Practice I
+ * CRN: [CHANGE THIS TO YOUR INFORMATION]
+ * Assignment: Employee
+ * 
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ * 
+ * @author Max Dokukin
+ */
 package edu.sbcc.cs105;
 
+/**
+ * This class provides first level testing the Employee object.
+ *
+ */
 public class Program {
 
+    /**
+     * Create an employee and test that the proper name has been created. Test
+     * the initial salary amount and then give the employee a raise. Then check
+     * to make sure the salary matches the raised salary.
+     * 
+     * 
+     * @param args
+     *            command line values. Not used in this example.
+     */
     public static void main(String[] args) {
-        System.out.println("Someday, I'll be a real program!");
-    }
+        Employee e = new Employee("John", 100);
 
+        System.out.println(e.getName() + "'s salary is : '" + e.getSalary());
+
+    }
 }
