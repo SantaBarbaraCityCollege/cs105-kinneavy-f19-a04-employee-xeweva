ff27f8ab7ebfef953b55d6b2265426135a4cfc3a
Max <xewe@MacBook.local>
2019-09-10 19:17:40 -0700

    commit msg

---
+++ src/main/java/edu/sbcc/cs105/Employee.java
@@ -0,0 +1,42 @@
+/**
+ * CS 105 Theory and Practice I
+ * CRN: [CHANGE THIS TO YOUR INFORMATION]
+ * Assignment: Employee
+ * 
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ * 
+ * @author Max Dokukin
+ */
+package edu.sbcc.cs105;
+
+/**
+ * This class implements an emplyee which is a person with a name and a salary.
+ *
+ */
+public class Employee {
+
+    String name;
+    double salary;
+
+    public Employee(String employeeName, double currentSalary) {
+        name = employeeName;
+        salary = currentSalary;
+    }
+
+    // Accessors that are obvious and have no side effects don't have to have
+    // any documentation unless you are creating a library to be used by other
+    // people.
+    public String getName() {
+		return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+
+    public void raiseSalary(double byPercent) {
+        salary *= (1 + byPercent / 100);
+    }
+}
